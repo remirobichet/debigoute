429d30cf8fb09493f00dd18df1b6e3dc71c52d6c
Rémi Robichet <remi.robichet@gmail.com>
2020-10-16 14:51:40 +0200

    :wrench: Update name in conf

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'debigoute',
+    title: 'Débigoute',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Calcul du débit en gouttes/minutes' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
